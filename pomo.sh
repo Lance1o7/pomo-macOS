f944827946189cc24a76ad7ab09a76a83617bf57
Lance <10292888+Lance1o7@users.noreply.github.com>
2024-11-25 15:32:58 -0500

    update for macOS

--- pomo.sh
+++ pomo.sh
@@ -1,13 +1,11 @@
-#!/usr/bin/env bash
-
 arg1=$1
 shift
 args="$*"
-                                               
+
 min=${arg1:?Example: pomo 15 Take a break}
-sec=$((min * 60))
+sec=$((min * 1))
 msg="${args:?Example: pomo 15 Take a break}"
 
 while true; do
-    date '+%H:%M' && sleep "${sec:?}" && notify-send -u critical -t 0 -a pomo "${msg:?}"
+    date '+%H:%M' && sleep "${sec:?}" && osascript -e "display notification \"${msg}\" with title \"Pomodoro\""
 done
